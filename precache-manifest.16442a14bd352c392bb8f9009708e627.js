ad786f8f669489327343610888b859df4c27f796
Dmitri Starunski <dmitriy.starunskiy@gmail.com>
2020-07-19 09:44:08 +0200

    Updates

---
+++ precache-manifest.16442a14bd352c392bb8f9009708e627.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e93764748134794ee14fce07ed337f",
+    "url": "/carousel-react/index.html"
+  },
+  {
+    "revision": "c0a06142e9fde3834142",
+    "url": "/carousel-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c15a32468e48348f1272",
+    "url": "/carousel-react/static/css/main.bf2538b3.chunk.css"
+  },
+  {
+    "revision": "c0a06142e9fde3834142",
+    "url": "/carousel-react/static/js/2.3e8ca824.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/carousel-react/static/js/2.3e8ca824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15a32468e48348f1272",
+    "url": "/carousel-react/static/js/main.86857d1c.chunk.js"
+  },
+  {
+    "revision": "bca91a4b68879c6a17a4",
+    "url": "/carousel-react/static/js/runtime-main.a97ab5f1.js"
+  },
+  {
+    "revision": "53200040e8adaf1af677e7cfe87d525b",
+    "url": "/carousel-react/static/media/easeofuse_icon.53200040.svg"
+  },
+  {
+    "revision": "ec262c21bab86791f6914589ee00c90f",
+    "url": "/carousel-react/static/media/pricing_icon.ec262c21.svg"
+  },
+  {
+    "revision": "bd07347d4ca7233f7bdffdb518fc7b6d",
+    "url": "/carousel-react/static/media/support_icon.bd07347d.svg"
+  }
+]);
